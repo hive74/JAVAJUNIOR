471f98d7e14ee16f2405b2f78616a5b4c1566305
Hive <stpe@inbox.ru>
2019-03-23 15:34:29 +0500

    1Array

---
+++ ololo1/src/multid_arrays.java
@@ -0,0 +1,24 @@
+public class multid_arrays {
+    public static void main(String[] args){
+        int[] number = {1,2,3};
+        int[][] matrice = {{1,2,3},
+                           {4,5,6},
+                           {7,8,9}};
+        System.out.println(matrice[2][0]); //7
+        System.out.println(matrice[1][2]); //6
+
+        int[] nubmer1 = new int[5];
+
+        String[][] strings = new String[2][3];
+        strings[0][1] = "Hello";
+        System.out.println(strings[0][1]);
+
+        for(int i = 0; i<matrice.length; i++){
+            for(int j = 0; j<matrice[i].length; j++){
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
