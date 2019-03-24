0829ab0a21bf7e39d91d0f528edcb6a2f7778f4b
Hive <stpe@inbox.ru>
2019-03-24 11:24:08 +0500

    1Array

--- ololo1/src/ClassesAndObjects.java
+++ ololo1/src/ClassesAndObjects.java
@@ -11,6 +11,10 @@ public class ClassesAndObjects {
         //person2.speak();
         person2.sayHello();
         person2.speak();
+        int year1 = person1.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Person1 to retire: " + year1 + " years");
+        System.out.println("Person2 to retire: " + year2 + " years");
     }
 }
 class Person{
@@ -18,8 +22,15 @@ class Person{
     String name;
     int age;
 
+    int calculateYearsToRetirement(){
+        int years = 65-age;
+        return years;
+        // System.out.println("Years to retirement: "+years);
+    }
+
+
     void speak(){
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 1; i++) {
             System.out.println("My name is " + name + ", I'm " + age + " y.o.");}
     }
     void sayHello(){
