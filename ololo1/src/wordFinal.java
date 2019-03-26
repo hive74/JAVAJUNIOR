56a18691e5fd64eaa365c4cb774e03101754ddfd
Hive <stpe@inbox.ru>
2019-03-26 22:52:10 +0500

    wordFinal

---
+++ ololo1/src/wordFinal.java
@@ -0,0 +1,18 @@
+public class wordFinal {
+    public static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
+        //X=5; dont work
+
+
+    }
+}
+
+class Test{
+    public static final int CONSTANT=0;
+
+    //public Test(int CONSTANT){
+    //   this.CONSTANT = CONSTANT;
+    }
+
+
