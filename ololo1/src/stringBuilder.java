531cdf26adde85f8b7ca3dc40e090b42c6ebc99b
Hive <stpe@inbox.ru>
2019-03-27 22:11:30 +0500

    String format

--- ololo1/src/stringBuilder.java
+++ ololo1/src/stringBuilder.java
@@ -12,6 +12,15 @@ public class stringBuilder {
         System.out.println(sb.toString());
         sb.append(" my").append(" kitty");
         System.out.println(sb.toString());
+
+        System.out.printf("Here %-10d This is a string, %s \n" , 111, "N_I_C_E");
+        System.out.printf("Here %-10d This is a string, %s \n", 123, "N_I_C_E");
+        System.out.printf("Here %10d This is a string, %s \n", 9876546, "N_I_C_E");
+        System.out.printf("Here %.3f This is a string \n", 17.123123123);
+
+
+
+
     }
 
 
