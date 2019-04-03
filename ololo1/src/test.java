daefdaac24c57331d6640db7cab619baf259f43d
Hive <stpe@inbox.ru>
2019-04-03 22:28:07 +0500

    Packege

---
+++ ololo1/src/test.java
@@ -0,0 +1,18 @@
+import Forest.Smth.SomeClass;
+import Forest.Squirell;
+import Forest.Tree;
+import Forest.*;
+
+import java.util.Scanner;
+
+
+public class test {
+
+    public static void main(String[] args) {
+        //Tree tree1 = new Tree();
+        //Squirell squirell1 = new Squirell();
+        Scanner scanner = new Scanner(System.in);
+        SomeClass smcl = new SomeClass();
+
+    }
+}
