4e1734d0d5843e6578c044d90a3d62df20809319
Hive <stpe@inbox.ru>
2019-04-08 23:02:20 +0500

    types

---
+++ ololo1/src/types.java
@@ -0,0 +1,12 @@
+public class types {
+    public static void main(String[] args) {
+        float f = 123.1f;
+        long l = 123l;
+
+        int x = (int)l; //yavno
+        long l1 = x; //neyavno
+
+    }
+
+
+}
