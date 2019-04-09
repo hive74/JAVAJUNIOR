01ff43af83c618adc39715b4d84f39018c2c78e7
Hive <stpe@inbox.ru>
2019-04-09 22:17:30 +0500

    updown

--- ololo1/src/test.java
+++ ololo1/src/test.java
@@ -7,12 +7,9 @@ import java.util.Scanner;
 
 
 public class test extends Person {
-
-
-
     public static void main(String[] args) {
 
-        Animal animal = new Animal();
+    /*    Animal animal = new Animal();
         animal.eat();
         Dog dog = new Dog();
         Cat cat = new Cat();
@@ -20,15 +17,21 @@ public class test extends Person {
         testik(animal);
         testik(cat);
         testik(dog);
-
-
-
     }
-
     public static void testik(Animal animal){
         animal.eat();
+    } */
+    //Upcasting - восходящее преобр
+        Dog dog = new Dog();
+        Animal animal1 = dog;
+
+        //Downcasting
+        Dog dog1 = (Dog) animal1;
+        dog1.bark();
     }
 }
+
+
 
 
 
